10df0f34b49f26421fe3c3f1f6305fbce3f0f02a
gustavopin <gustavorp96@gmail.com>
2025-02-20 17:06:54 -0300

    Included new categories (Population density and SUPER POWER) to the cards, as well as a comparison to see which card won (See README for details)

--- trunfo.c
+++ trunfo.c
@@ -2,8 +2,12 @@
 
 int main() {
     //variables
-    int pop1, pop2, point1, point2;
+    int spot1, spot2;
+    int res_pop, res_area, res_spot;
+    unsigned long int pop1, pop2;
     float pib1, pib2, area1, area2, dens1, dens2, percapita1, percapita2;
+    float super1, super2;
+    float res_pib, res_dens, res_percapita, res_super;
     char city1[50], city2[50], code1[10], code2[10];
     char state1, state2;
 
@@ -26,17 +30,20 @@ int main() {
     scanf("%d", &pop1);
     printf("City Area (km2): ");
     scanf("%f", &area1);
-    printf("Brute Internal Product (billions): ");
+    printf("Brute Internal Product (in billions): ");
     scanf("%f", &pib1);
     printf("Number of Turistic Spots: ");
-    scanf("%d", &point1);
+    scanf("%d", &spot1);
 
     //calculating population density and BIP per capita of CARD 1
     //population density
-    dens1 = (float) pop1/area1;
+    dens1 = (float)pop1/(float)area1;
 
     //BIP per capita
-    percapita1 = (float) pib1/pop1;
+    percapita1 = (pib1*1000000000)/(float)pop1;
+
+    //calculating super power value
+    super1 = (float)pop1 + (float)area1 + pib1 + (float)spot1 + percapita1 + (1/dens1);
 
     //card2
     printf("\n\nBellow you will type the informations about Card 2: \n\n");
@@ -53,15 +60,18 @@ int main() {
     printf("Brute Internal Product (in billions): ");
     scanf("%f", &pib2);
     printf("Number of Turistic Spots: ");
-    scanf("%d", &point2);
+    scanf("%d", &spot2);
 
     //calculating population density and BIP per capita of CARD 2
     //population density
-    dens2 = (float) pop2/area2;
+    dens2 = (float)pop2/(float)area2;
 
     //BIP per capita
-    percapita2 = (float) pib2/pop2;
+    percapita2 = (pib2*1000000000)/(float)pop2;
     printf("\n\nThe cards you created are: ");
+
+    //calculating super power value
+    super2 = (float)pop2 + (float)area2 + pib2 + (float)spot2 + percapita2 + (1/dens2);
 
     //printing the information card1
     printf("\n\nCard Code: %c%s\n", state1, code1);
@@ -70,9 +80,10 @@ int main() {
     printf("Population: %d\n", pop1);
     printf("City Area: %.2f\n", area1);
     printf("BIP: %.2f billions of BRL\n", pib1);
-    printf("Turistic Spots: %d\n", point1);
+    printf("Turistic Spots: %d\n", spot1);
     printf("Population Density: %.2f hab/km2\n", dens1);
     printf("BIP per capita: %.2f BRL\n", percapita1);
+    printf("SUPER POWER: %.2f\n", super1);
 
     //printing the information card2
     printf("\nCard Code: %c%s\n", state2, code2);
@@ -81,9 +92,29 @@ int main() {
     printf("Population: %d\n", pop2);
     printf("City Area: %.2f\n", area2);
     printf("BIP: %.2f billions of BRL\n", pib2);
-    printf("Turistic Spots: %d\n", point2);
-    printf("Population Density: %.2f hab/km2\n", dens1);
-    printf("BIP per capita: %.2f BRL\n", percapita1);
+    printf("Turistic Spots: %d\n", spot2);
+    printf("Population Density: %.2f hab/km2\n", dens2);
+    printf("BIP per capita: %.2f BRL\n", percapita2);
+    printf("SUPER POWER: %.2f\n", super2);
     
+    //Card comparison
+    //this will determine what card wins in that category, 1 will represent the WIN and to CARD1 and 0 will represent the WIN to CARD2
+    res_pop = pop1 > pop2;
+    res_area = area1 > area2;
+    res_pib = pib1 > pib2;
+    res_spot = spot1 > spot2;
+    res_dens = dens1 < dens2;
+    res_percapita = percapita1 > percapita2;
+    res_super = super1 > super2;
+
+    //results for the comparison
+    printf("\n\nPopulation: %d", res_pop);
+    printf("\nArea: %d", res_area);
+    printf("\nBIP: %.0f", res_pib);
+    printf("\nTuristic Spots: %d", res_spot);
+    printf("\nPopulation Density: %.0f", res_dens);
+    printf("\nBIP per capita: %.0f", res_percapita);
+    printf("\nSUPER POWER: %.0f", res_super);
+
     return 0;
 }
